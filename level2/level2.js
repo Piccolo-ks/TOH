0b645d0b70a3a396cc5bc9966da30d2c222d592d
Piccolo-ks <67235002+Piccolo-ks@users.noreply.github.com>
2020-08-19 02:06:56 +0200

    TOH

---
+++ level2/level2.js
@@ -0,0 +1,41 @@
+'use strict';
+
+$(function(){
+
+  // $('[data-block]').draggable();
+//
+
+// ['data-stack=3'].insertAdjacentHTML('afterEnd', '<button onclick="resetButton()" id="reset" type="button">Reset</button>');
+
+  $('[data-block]').draggable({
+    revert: 'invalid'
+  });
+  $('[data-stack]').droppable({
+    drop: function(event, ui){
+      let $last = ($(this).children().last()).data('block');
+      let $dragging = $(ui.draggable).data('block');
+      if($dragging > $last){
+        $(ui.draggable).draggable('option', 'revert', true);
+      }else{
+        $(ui.draggable).appendTo(this).attr('style', 'position: "relative"');
+      }
+      checkWin();
+      //checks winning condition after each drop
+  }
+  });
+
+  function checkWin(){
+    console.log($('[data-stack=3] div').length)
+    if($('[data-stack=3] div').length === 5){
+      $('#announce-game-won').after('<button><a href="../level3/level3.html">LEVEL 3</a></button>');
+      // alert('User Wins!');
+    }
+    }
+    $('[data-stack=3]').after('<input type="button" id="resetButton" value="Reset"/>'
+  );
+
+    $('#resetButton').click(function(){
+        location.reload();
+      })
+
+    })
